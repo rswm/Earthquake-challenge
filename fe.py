5382b66a152be932b0d472c1db03f3c6c13f0c7b
= <rswmallett@gmail.com>
2024-02-01 13:39:38 +0000

    Added fe.py

---
+++ fe.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+def FeatureEngineering():
+    
+    #Import data
+
+    train_values = 'data/train_values.csv'
+    train_labels  = 'data/train_labels.csv'
+
+    #Load data
+
+    tv = pd.read_csv(train_values)
+    tl = pd.read_csv(train_labels)
+    
+    #Merge data
+    cdf = pd.merge(df, targetdf, left_index=True, right_index=True)
+    
+    #Drop missing target values
+
+    cdf  = combcdfdf.dropna(subset=['damage_grade'])
+
+    #More feature engineering will be done here
+
+    return(cdf)
