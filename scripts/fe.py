412d7f56e4393d62b92a5f1fac8c0f28a84b48cb
= <rswmallett@gmail.com>
2024-02-02 11:15:30 +0000

    updated fe

--- scripts/fe.py
+++ scripts/fe.py
@@ -1,44 +1,37 @@
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
 
 def FeatureEngineering():
     
     #Import data
 
-    train_values = 'data/train_values.csv'
-    train_labels  = 'data/train_labels.csv'
-    test_values = 'data/test_values.csv'
-    
+    train_values = '../data/train_values.csv'
+    train_labels  = '../data/train_labels.csv'
+    test_values = '../data/test_values.csv'
 
     #Load data
 
     tv = pd.read_csv(train_values)
     tl = pd.read_csv(train_labels)
-    testv = pd.read_csv(test_values)
+    testdf = pd.read_csv(test_values)
 
     #Merge data
-    cdf = pd.merge(tv, tl, left_index=True, right_index=True)
+    cdf = tv.join(tl.set_index('building_id'), on='building_id')
     
-    #Drop missing target values
-
-    cdf  = cdf.dropna(subset=['damage_grade'])
-
     #Feature engineering:
-    
+    #Mean encode geo levels 1, 2 and 3
+
+    mean_encoding1 = cdf.groupby('geo_level_1_id')['damage_grade'].mean()
+    mean_encoding2 = cdf.groupby('geo_level_2_id')['damage_grade'].mean()
+    mean_encoding3 = cdf.groupby('geo_level_3_id')['damage_grade'].mean()
+
+    cdf['geo_level_1_id_mean_encoded'] = cdf['geo_level_1_id'].map(mean_encoding1)
+    cdf['geo_level_2_id_mean_encoded'] = cdf['geo_level_2_id'].map(mean_encoding2)
+    cdf['geo_level_3_id_mean_encoded'] = cdf['geo_level_3_id'].map(mean_encoding3)
+    testdf['geo_level_1_id_mean_encoded'] = testdf['geo_level_1_id'].map(mean_encoding1)
+    testdf['geo_level_2_id_mean_encoded'] = testdf['geo_level_2_id'].map(mean_encoding2)
+    testdf['geo_level_3_id_mean_encoded'] = testdf['geo_level_3_id'].map(mean_encoding3)
+
+    return cdf, testdf
 
 
 
-    #Encode categoricals 
-
-    #for column in df.columns:
-    #    if df[column].dtype == 'object' or df[column].dtype.name == 'category':
-    #        df[column] = pd.factorize(df[column])[0]
-
-
-    #Label encode geo levels
-
-    cdf['geo_level_1_id'] = cdf['geo_level_1_id'].astype(object)
-    cdf['geo_level_2_id'] = cdf['geo_level_2_id'].astype(object)
-    cdf['geo_level_3_id'] = cdf['geo_level_3_id'].astype(object)
-    
-    return cdf
