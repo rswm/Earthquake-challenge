47b57e9550c5bed16efa3bf124be22a3ae8a0f52
= <rswmallett@gmail.com>
2024-02-02 09:24:12 +0000

    change geo level types to objects

--- scripts/fe.py
+++ scripts/fe.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 def FeatureEngineering():
     
@@ -6,12 +7,15 @@ def FeatureEngineering():
 
     train_values = 'data/train_values.csv'
     train_labels  = 'data/train_labels.csv'
+    test_values = 'data/test_values.csv'
+    
 
     #Load data
 
     tv = pd.read_csv(train_values)
     tl = pd.read_csv(train_labels)
-    
+    testv = pd.read_csv(test_values)
+
     #Merge data
     cdf = pd.merge(tv, tl, left_index=True, right_index=True)
     
@@ -19,6 +23,15 @@ def FeatureEngineering():
 
     cdf  = cdf.dropna(subset=['damage_grade'])
 
-    #More feature engineering will be done here
+    #Feature engineering:
+    
+    #Label encode geo levels
 
-    return(cdf)
+    cdf['geo_level_1_id'] = cdf['geo_level_1_id'].astype(object)
+    cdf['geo_level_2_id'] = cdf['geo_level_2_id'].astype(object)
+    cdf['geo_level_3_id'] = cdf['geo_level_3_id'].astype(object)
+
+
+    return cdf
+
+FeatureEngineering()
